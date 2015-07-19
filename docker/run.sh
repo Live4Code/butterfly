1387dc1dff47b2c04eb54885ee5f3dc724a43093
Ming He <meticulous.dft@gmail.com>
2015-07-18 22:12:49 -0700

    terminal playgroud

--- docker/run.sh
+++ docker/run.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 
-# Set password
-echo "root:${PASSWORD}" | chpasswd
-
 if [ -z ${PORT} ]
 then
   echo "Starting on default port: 57575"
-  /opt/app/butterfly.server.py --unsecure --host=0.0.0.0
+  /home/terminal/app/butterfly.server.py --unsecure --host=0.0.0.0
 else
   echo "Starting on port: ${PORT}"
-  /opt/app/butterfly.server.py --unsecure --host=0.0.0.0 --port=${PORT}
+  /home/terminal/app/butterfly.server.py --unsecure --host=0.0.0.0 --port=${PORT}
 fi
